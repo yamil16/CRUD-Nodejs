c32ffcbe9d9ea4891c8be7dd85db3752e66fb651
yamil16 <31110277+yamil16@users.noreply.github.com>
2019-05-21 15:09:23 -0300

    Initial commit

---
+++ src/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'localhost',
+        //user: 'root',
+        user: 'yamil',
+        password: 'Facultad',
+        database: 'db_links'
+    }
+
+};
